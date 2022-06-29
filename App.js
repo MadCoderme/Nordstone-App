30587cc99aab82c0ce3f1fcd8f340ebd597d3dc9
MadCoderme <youcanreplylishup@gmail.com>
2022-06-29 23:45:58 +0600

    initial commit

---
+++ App.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+
+//screens
+import Notification from './src/Notification'
+
+
+const App = () => {
+  const isDarkMode = useColorScheme() === 'dark'
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  }
+
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Notification" component={Notification} />
+      <Tab.Screen name="Photo" component={SettingsScreen} />
+      <Tab.Screen name="Text" component={HomeScreen} />
+      <Tab.Screen name="Calculator" component={SettingsScreen} />
+    </Tab.Navigator>
+  )
+}
+
+export default App
