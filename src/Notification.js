c487893767484d1d3a4e618d015cd1841cdc6009
MadCoderme <63955762+MadCoderme@users.noreply.github.com>
2022-06-30 09:48:53 +0600

    Update Notification.js

--- src/Notification.js
+++ src/Notification.js
@@ -5,15 +5,34 @@ import {
   StatusBar,
   StyleSheet,
   Text,
-  useColorScheme,
+  TouchableOpacity,
   View,
 } from 'react-native'
+import PushNotification from "react-native-push-notification"
 
+// C46D5E, DAA588, F3C98B, 9CF6F6
 
 export default function Notification() {
+
+    function send() {
+      PushNotification.localNotification({
+        title: "Nordstone Alert!", 
+        message: "Hello world, this is your super important notification, read or lose",
+      });
+    }
+    
     return (
-        <SafeAreaView>
-            
+        <SafeAreaView style={{flex: 1, justifyContent: 'center',
+        alignItems: 'center'}}>
+            <TouchableOpacity onPress={send} style={{
+              backgroundColor: '#F56960',
+              padding: 20,
+              borderRadius: 10
+            }}>
+              <Text style={{color: 'white', fontSize: 20}}>
+                Send Notification
+              </Text>
+            </TouchableOpacity>
         </SafeAreaView>
     )
 }
